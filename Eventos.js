97fb64a36e382854a0724d131eaba0dff399274a
Juan-Jorge <juan>
2022-01-20 00:57:38 -0300

    first commit

---
+++ Eventos.js
@@ -0,0 +1,23 @@
+let DataDoEvento = 21/01/2022  
+if (DataDoEvento > 20/01/2022) {
+    console.log ("Sua data esta valida")
+} else {
+    console.log("Sua data esta invalida")
+}
+//se o participante for maior que 18 permitir o cadastro
+
+let Idade = 18
+if (Idade>=18) {
+    console.log ("Acesso permitido")
+}else {
+    console.log ("Idade não permitida")
+}
+//A quantidade de participantes tem que ser menor que 100
+
+let participantes = [Juan,Jorge,Joel,Jaqueline,Julia,Isabela]
+let quantidadedeparticipantes = participantes.length
+if (quantidadedeparticipantes <100) {
+    console.log ("Acesso negado!")
+}else {
+    console.log ("Acesso permitido!")
+}
